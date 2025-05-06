660f5f6a3b043293ce1e334e97e433da63a30008
Pritirekha panda <102647459+Pritirekha23@users.noreply.github.com>
2025-05-06 11:46:27 +0530

    database setup

--- ShaftSeal_Website/settings.py
+++ ShaftSeal_Website/settings.py
@@ -85,10 +85,19 @@ DATABASES = {
 } """
 
 
+# Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # This stores the DB in your project's base directory
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+         'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
