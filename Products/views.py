97dd4b4c15ec7448c522123de6b229106820d7b7
Darshan756 <144026839+Darshan756@users.noreply.github.com>
2025-05-08 12:15:57 +0530

    chaged home page

--- Products/views.py
+++ Products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_page
+from django.http import HttpResponse,Http404
+
 
  # cache this view for 15 minutes (900 seconds)
 
@@ -9,6 +11,22 @@ from django.views.decorators.cache import cache_page
 
 
 def home(request):
+    search_data = request.GET.get('q','').lower()
+    
+    if search_data:
+        try:
+            if 'spares' in search_data:
+                return redirect('spares_page')
+            elif 'product' in search_data:
+                return redirect('products')
+            elif 'energy' in search_data or 'efficiency' in search_data:
+                return redirect('Energy_efficiency_home')
+            else:
+                return HttpResponse("Invalid search keyword.", status=400)
+
+        except Exception as e:
+             return HttpResponse("An error occurred.", status=500)
+           
     return render(request, 'Products/home.html')
    
 cache_page(60*15)
